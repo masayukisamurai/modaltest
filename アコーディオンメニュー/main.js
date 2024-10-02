ebb083ff6a9284183e00e2ef57e57fa0e74ae6d2
masayukisamurai <maaboo0812@gmail.com>
2024-10-02 23:47:49 +0900

    アコーディオンメニュー作成中

---
+++ アコーディオンメニュー/main.js
@@ -0,0 +1,7 @@
+const test = document.getElementsByClassName('test');
+
+const mainmenu = document.getElementById('mainmenu');
+
+mainmenu.addEventListener('click', () => {
+  test.style.display = block;
+});
